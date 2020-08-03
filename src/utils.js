77e19a03588cc19aaccc594d8a9e68d5b778aaad
bruno foggia <me@brunofoggia.com.br>
2020-08-03 13:54:16 -0300

    added catch paste fn

--- src/utils.js
+++ src/utils.js
@@ -24,4 +24,31 @@ utils.when = function (waitFunction, afterWait, interval) {
     afterWait();
 };
 
+/**
+ * Catch paste value
+ * @param evt event
+ * @param elem function
+ * @param interval integer microseconds interval between checks
+ */
+utils.catchPaste = function (e, el, c) {
+    if (navigator.clipboard && navigator.clipboard.readText) {
+      // modern approach with Clipboard API
+      navigator.clipboard.readText().then(c);
+    } else if (e.originalEvent && e.originalEvent.clipboardData) {
+      // OriginalEvent is a property from jQuery, normalizing the event object
+      c(e.originalEvent.clipboardData.getData('text'));
+    } else if (e.clipboardData) {
+      // used in some browsers for clipboardData
+      c(e.clipboardData.getData('text/plain'));
+    } else if (window.clipboardData) {
+      // Older clipboardData version for Internet Explorer only
+      c(window.clipboardData.getData('Text'));
+    } else {
+      // Last resort fallback, using a timer
+      setTimeout(() => {
+        c(el.value)
+      }, 100);
+    }
+};
+
 export default utils;
