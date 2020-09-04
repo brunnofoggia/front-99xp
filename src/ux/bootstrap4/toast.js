65ba7cbbd4507f4049ca670a2defb9df2f44bddd
bruno foggia <me@brunofoggia.com.br>
2020-09-04 15:00:21 -0300

    toast improvements

--- src/ux/bootstrap4/toast.js
+++ src/ux/bootstrap4/toast.js
@@ -1,21 +1,24 @@
 import _ from 'underscore-99xp';
 import toastTpl from './toast.jst';
 
-var setContainer = function() {
+var setContainer = function(opts) {
     var $p = $('body > .toasts');
-            
-    !$p.length && ($p = $('<div class="toasts">').appendTo($('body')).css('position', 'fixed').css('bottom', '10px').css('right', '10px'));
-    
+
+    !$p.length && ($p = $('<div class="toasts">').appendTo($('body')).css('position', 'fixed').css(opts.positionY || 'bottom', '10px').css(opts.positionX || 'right', '10px'));
+
     return $p;
 }
 
 export default function(opts={}) {
-    var tpl = toastTpl(_.extend({color: 'info'}, opts)), $p = setContainer(), $el;
-    
+    var $p = $p = setContainer(opts);
+    if(!opts.msg) { return; }
+
+    var tpl = toastTpl(_.extend({color: 'info'}, opts)), $el;
+
 
     $el = $(tpl).appendTo($p);
-    $el.toast(_.defaults(opts, {autohide: true, delay: 2500})).toast('show');
-    setTimeout(_.partial(($el)=>{
+    $el.toast(_.defaults(opts, {close: false, autohide: true, delay: opts.delay || 2500})).toast('show');
+    opts.autohide && setTimeout(_.partial(($el)=>{
         $el.remove();
-    }, $el), 3000);
+    }, $el), opts.delay + 500);
 }
