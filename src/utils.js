b9d3891926f9700e5c65b2e85c4c8e36b8aa7e78
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-03-15 18:16:08 -0300

    added blob to link utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 /**
- * 
+ *
  */
-import _ from 'underscore-99xp';
+import _ from "underscore-99xp";
+import Bb from "backbone";
 
 var utils = {};
 
@@ -11,20 +12,50 @@ var utils = {};
  * @param afterWait function
  * @param interval integer microseconds interval between checks
  */
-utils.when = function (waitFunction, afterWait, interval, afterPassed, passed=0) {
+utils.when = function (
+    waitFunction,
+    afterWait,
+    interval,
+    afterPassed,
+    passed = 0
+) {
     var test = waitFunction();
     !interval && (interval = 200);
 
     if (!test) {
-        let afterPassedEndProccess = typeof afterPassed === 'function' ? afterPassed(passed) : false;
-        if(afterPassedEndProccess===true) {
+        let afterPassedEndProccess =
+            typeof afterPassed === "function" ? afterPassed(passed) : false;
+        if (afterPassedEndProccess === true) {
             return;
         }
 
-        return _.delay(_.bind(function (waitFunction, afterWait, interval, afterPassed, passed) {
-            passed += interval;
-            utils.when(waitFunction, afterWait, interval, afterPassed, passed);
-        }, this, waitFunction, afterWait, interval, afterPassed, passed), interval);
+        return _.delay(
+            _.bind(
+                function (
+                    waitFunction,
+                    afterWait,
+                    interval,
+                    afterPassed,
+                    passed
+                ) {
+                    passed += interval;
+                    utils.when(
+                        waitFunction,
+                        afterWait,
+                        interval,
+                        afterPassed,
+                        passed
+                    );
+                },
+                this,
+                waitFunction,
+                afterWait,
+                interval,
+                afterPassed,
+                passed
+            ),
+            interval
+        );
     }
 
     afterWait();
@@ -38,38 +69,68 @@ utils.when = function (waitFunction, afterWait, interval, afterPassed, passed=0)
  */
 utils.catchPaste = function (e, el, c) {
     if (navigator.clipboard && navigator.clipboard.readText) {
-      // modern approach with Clipboard API
-      navigator.clipboard.readText().then(c);
+        // modern approach with Clipboard API
+        navigator.clipboard.readText().then(c);
     } else if (e.originalEvent && e.originalEvent.clipboardData) {
-      // OriginalEvent is a property from jQuery, normalizing the event object
-      c(e.originalEvent.clipboardData.getData('text'));
+        // OriginalEvent is a property from jQuery, normalizing the event object
+        c(e.originalEvent.clipboardData.getData("text"));
     } else if (e.clipboardData) {
-      // used in some browsers for clipboardData
-      c(e.clipboardData.getData('text/plain'));
+        // used in some browsers for clipboardData
+        c(e.clipboardData.getData("text/plain"));
     } else if (window.clipboardData) {
-      // Older clipboardData version for Internet Explorer only
-      c(window.clipboardData.getData('Text'));
+        // Older clipboardData version for Internet Explorer only
+        c(window.clipboardData.getData("Text"));
     } else {
-      // Last resort fallback, using a timer
-      setTimeout(() => {
-        c(el.value)
-      }, 100);
+        // Last resort fallback, using a timer
+        setTimeout(() => {
+            c(el.value);
+        }, 100);
     }
 };
 
 utils.addCssTag = function (o) {
-    !_.isObject(o) && (o = {href: o});
+    !_.isObject(o) && (o = { href: o });
     o = _.defaults(o, {
-        rel: 'stylesheet',
-        type: 'text/css'
+        rel: "stylesheet",
+        type: "text/css",
     });
 
-    var link = document.createElement('link');
+    var link = document.createElement("link");
     _.each(o, (v, k) => {
         link[k] = v;
     });
 
     document.head.appendChild(link);
-}
+};
+
+window.URL = window.URL || window.webkitURL;
+utils.openBlobPDF = function (url) {
+    Bb.ajax({
+        url: url,
+        method: "GET",
+        xhrFields: {
+            responseType: "blob",
+        },
+        success(response) {
+            utils.openBlob(response);
+        },
+    });
+};
+
+utils.openBlob = function (content, type, filename) {
+    if (type) content = new Blob(content, { type });
+
+    var fileURL = URL.createObjectURL(content);
+    if (!filename) window.open(fileURL);
+    else {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = fileURL;
+        a.download = filename;
+        a.click();
+        window.URL.revokeObjectURL(fileURL);
+    }
+};
 
 export default utils;
