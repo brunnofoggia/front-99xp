27041d3ece1c787e654d0214f454411258a17dd2
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-06-24 13:07:50 -0300

    ucfirst fix

--- src/extend.js
+++ src/extend.js
@@ -1,9 +1,9 @@
-
 /**
- * 
+ *
  */
 Object.size = function (obj) {
-    var size = 0, key;
+    var size = 0,
+        key;
     for (key in obj) {
         if (obj.hasOwnProperty(key)) {
             size++;
@@ -14,7 +14,8 @@ Object.size = function (obj) {
 };
 
 Function.prototype.extends = function (parent) {
-    var prototype = parent.prototype, create = Object.create(prototype);
+    var prototype = parent.prototype,
+        create = Object.create(prototype);
     this.prototype = create;
     this.prototype.parent = parent;
 };
@@ -22,7 +23,7 @@ Function.prototype.extends = function (parent) {
 String.prototype.matchAll = function (regexp) {
     var matches = [];
     this.replace(regexp, function () {
-        var arr = ([]).slice.call(arguments, 0);
+        var arr = [].slice.call(arguments, 0);
         var extras = arr.splice(-2);
         arr.index = extras[0];
         arr.input = extras[1];
@@ -32,17 +33,22 @@ String.prototype.matchAll = function (regexp) {
 };
 
 String.prototype.ucfirst = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
 
 String.prototype.regexIndexOf = function (regex, startpos) {
     var indexOf = this.substring(startpos || 0).search(regex);
-    return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+    return indexOf >= 0 ? indexOf + (startpos || 0) : indexOf;
 };
 
 String.prototype.regexLastIndexOf = function (regex, startpos) {
-    regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiLine ? "m" : ""));
-    if (typeof (startpos) == "undefined") {
+    regex = regex.global
+        ? regex
+        : new RegExp(
+              regex.source,
+              "g" + (regex.ignoreCase ? "i" : "") + (regex.multiLine ? "m" : "")
+          );
+    if (typeof startpos == "undefined") {
         startpos = this.length;
     } else if (startpos < 0) {
         startpos = 0;
@@ -58,31 +64,82 @@ String.prototype.regexLastIndexOf = function (regex, startpos) {
 };
 
 String.prototype.toDate = function () {
-    var data = this.toString().split(' '),
-            dateStr = data[0].split('-'),
-            timeStr = data[1] ? data[1].split(':') : [0, 0, 0], date;
+    var data = this.toString().split(" "),
+        dateStr = data[0].split("-"),
+        timeStr = data[1] ? data[1].split(":") : [0, 0, 0],
+        date;
 
-    date = new Date(dateStr[0], dateStr[1] - 1, dateStr[2], timeStr[0], timeStr[1], timeStr[2]);
+    date = new Date(
+        dateStr[0],
+        dateStr[1] - 1,
+        dateStr[2],
+        timeStr[0],
+        timeStr[1],
+        timeStr[2]
+    );
     return date;
 };
 
 Date.prototype.hash = function () {
     var date = this;
-    return [date.getYear(), date.getMonth(), date.getDay(), date.getHours(), date.getMinutes(), date.getSeconds()].join('');
+    return [
+        date.getYear(),
+        date.getMonth(),
+        date.getDay(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+    ].join("");
 };
 
 Date.prototype.format = function (format, utc) {
     var date = this;
-    var MMMM = ["\x00", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var MMM = ["\x01", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var dddd = ["\x02", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var MMMM = [
+        "\x00",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    var MMM = [
+        "\x01",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    var dddd = [
+        "\x02",
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
     var ddd = ["\x03", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     function ii(i, len) {
         var s = i + "";
         len = len || 2;
-        while (s.length < len)
-            s = "0" + s;
+        while (s.length < len) s = "0" + s;
         return s;
     }
 
@@ -162,20 +219,26 @@ String.prototype.toNumber = function () {
 
 /**
  * Number.prototype.format(n, x, s, c)
- * 
+ *
  * @param integer n: length of decimal
  * @param integer x: length of whole part
  * @param mixed   s: sections delimiter
  * @param mixed   c: decimal delimiter
  */
 Number.prototype.format = function (n, x, s, c) {
-    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-            num = this.toFixed(Math.max(0, ~~n));
+    var re = "\\d(?=(\\d{" + (x || 3) + "})+" + (n > 0 ? "\\D" : "$") + ")",
+        num = this.toFixed(Math.max(0, ~~n));
 
-    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+    return (c ? num.replace(".", c) : num).replace(
+        new RegExp(re, "g"),
+        "$&" + (s || ",")
+    );
 };
 
-Base64 = {_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode: function (e) {
+Base64 = {
+    _keyStr:
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (e) {
         var t = "";
         var n, r, i, s, o, u, a;
         var f = 0;
@@ -185,18 +248,24 @@ Base64 = {_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567
             r = e.charCodeAt(f++);
             i = e.charCodeAt(f++);
             s = n >> 2;
-            o = (n & 3) << 4 | r >> 4;
-            u = (r & 15) << 2 | i >> 6;
+            o = ((n & 3) << 4) | (r >> 4);
+            u = ((r & 15) << 2) | (i >> 6);
             a = i & 63;
             if (isNaN(r)) {
-                u = a = 64
+                u = a = 64;
             } else if (isNaN(i)) {
-                a = 64
+                a = 64;
             }
-            t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+            t =
+                t +
+                this._keyStr.charAt(s) +
+                this._keyStr.charAt(o) +
+                this._keyStr.charAt(u) +
+                this._keyStr.charAt(a);
         }
-        return t
-    }, decode: function (e) {
+        return t;
+    },
+    decode: function (e) {
         var t = "";
         var n, r, i;
         var s, o, u, a;
@@ -207,89 +276,96 @@ Base64 = {_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567
             o = this._keyStr.indexOf(e.charAt(f++));
             u = this._keyStr.indexOf(e.charAt(f++));
             a = this._keyStr.indexOf(e.charAt(f++));
-            n = s << 2 | o >> 4;
-            r = (o & 15) << 4 | u >> 2;
-            i = (u & 3) << 6 | a;
+            n = (s << 2) | (o >> 4);
+            r = ((o & 15) << 4) | (u >> 2);
+            i = ((u & 3) << 6) | a;
             t = t + String.fromCharCode(n);
             if (u != 64) {
-                t = t + String.fromCharCode(r)
+                t = t + String.fromCharCode(r);
             }
             if (a != 64) {
-                t = t + String.fromCharCode(i)
+                t = t + String.fromCharCode(i);
             }
         }
         t = Base64._utf8_decode(t);
-        return t
-    }, _utf8_encode: function (e) {
+        return t;
+    },
+    _utf8_encode: function (e) {
         e = e.replace(/\r\n/g, "\n");
         var t = "";
         for (var n = 0; n < e.length; n++) {
             var r = e.charCodeAt(n);
             if (r < 128) {
-                t += String.fromCharCode(r)
+                t += String.fromCharCode(r);
             } else if (r > 127 && r < 2048) {
-                t += String.fromCharCode(r >> 6 | 192);
-                t += String.fromCharCode(r & 63 | 128)
+                t += String.fromCharCode((r >> 6) | 192);
+                t += String.fromCharCode((r & 63) | 128);
             } else {
-                t += String.fromCharCode(r >> 12 | 224);
-                t += String.fromCharCode(r >> 6 & 63 | 128);
-                t += String.fromCharCode(r & 63 | 128)
+                t += String.fromCharCode((r >> 12) | 224);
+                t += String.fromCharCode(((r >> 6) & 63) | 128);
+                t += String.fromCharCode((r & 63) | 128);
             }
         }
-        return t
-    }, _utf8_decode: function (e) {
+        return t;
+    },
+    _utf8_decode: function (e) {
         var t = "";
         var n = 0;
-        var r = c1 = c2 = 0;
+        var r = (c1 = c2 = 0);
         while (n < e.length) {
             r = e.charCodeAt(n);
             if (r < 128) {
                 t += String.fromCharCode(r);
-                n++
+                n++;
             } else if (r > 191 && r < 224) {
                 c2 = e.charCodeAt(n + 1);
-                t += String.fromCharCode((r & 31) << 6 | c2 & 63);
-                n += 2
+                t += String.fromCharCode(((r & 31) << 6) | (c2 & 63));
+                n += 2;
             } else {
                 c2 = e.charCodeAt(n + 1);
                 c3 = e.charCodeAt(n + 2);
-                t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
-                n += 3
+                t += String.fromCharCode(
+                    ((r & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+                );
+                n += 3;
             }
         }
-        return t
-    }};
+        return t;
+    },
+};
 
-window.getQueryVariable = function(variable) {
+window.getQueryVariable = function (variable) {
     var query = window.location.search.substring(1);
-    var vars = query.split('&');
+    var vars = query.split("&");
     for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
+        var pair = vars[i].split("=");
         if (decodeURIComponent(pair[0]) == variable) {
             return decodeURIComponent(pair[1]);
         }
     }
 
-//    console.log('Query variable %s not found', variable);
-}
-
-window.urldecode = function(str) {
-   return decodeURIComponent((str+'').replace(/\+/g, '%20'));
-}
-
-String.prototype.camelize = function () {
-    return this.replace(/^([A-Z])|[\s-_]+(\w)/g, function(match, p1, p2, offset) {
-        if (p2) return p2.toUpperCase();
-        return p1.toLowerCase();        
-    });
+    //    console.log('Query variable %s not found', variable);
 };
 
-String.prototype.decamelize = function (str, separator='-'){
-	return this
-        .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
-        .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + separator + '$2')
+window.urldecode = function (str) {
+    return decodeURIComponent((str + "").replace(/\+/g, "%20"));
+};
+
+String.prototype.camelize = function () {
+    return this.replace(
+        /^([A-Z])|[\s-_]+(\w)/g,
+        function (match, p1, p2, offset) {
+            if (p2) return p2.toUpperCase();
+            return p1.toLowerCase();
+        }
+    );
+};
+
+String.prototype.decamelize = function (str, separator = "-") {
+    return this.replace(/([a-z\d])([A-Z])/g, "$1" + separator + "$2")
+        .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, "$1" + separator + "$2")
         .toLowerCase();
-}
+};
 
 String.prototype.pad = function (pad_length, pad_string, pad_type) {
     //  discuss at: http://phpjs.org/functions/str_pad/
@@ -302,12 +378,12 @@ String.prototype.pad = function (pad_length, pad_string, pad_type) {
     //   example 2: str_pad('Kevin van Zonneveld', 30, '-', 'STR_PAD_BOTH');
     //   returns 2: '------Kevin van Zonneveld-----'
 
-    var half = '',
-            pad_to_go;
+    var half = "",
+        pad_to_go;
 
     var str_pad_repeater = function (s, len) {
-        var collect = '',
-                i;
+        var collect = "",
+            i;
 
         while (collect.length < len) {
             collect += s;
@@ -317,18 +393,22 @@ String.prototype.pad = function (pad_length, pad_string, pad_type) {
         return collect;
     };
 
-    input = ''+this;
-    pad_string = pad_string !== undefined ? pad_string : ' ';
+    input = "" + this;
+    pad_string = pad_string !== undefined ? pad_string : " ";
 
-    if (pad_type !== 'STR_PAD_LEFT' && pad_type !== 'STR_PAD_RIGHT' && pad_type !== 'STR_PAD_BOTH') {
-        pad_type = 'STR_PAD_RIGHT';
+    if (
+        pad_type !== "STR_PAD_LEFT" &&
+        pad_type !== "STR_PAD_RIGHT" &&
+        pad_type !== "STR_PAD_BOTH"
+    ) {
+        pad_type = "STR_PAD_RIGHT";
     }
     if ((pad_to_go = pad_length - input.length) > 0) {
-        if (pad_type === 'STR_PAD_LEFT') {
+        if (pad_type === "STR_PAD_LEFT") {
             input = str_pad_repeater(pad_string, pad_to_go) + input;
-        } else if (pad_type === 'STR_PAD_RIGHT') {
+        } else if (pad_type === "STR_PAD_RIGHT") {
             input = input + str_pad_repeater(pad_string, pad_to_go);
-        } else if (pad_type === 'STR_PAD_BOTH') {
+        } else if (pad_type === "STR_PAD_BOTH") {
             half = str_pad_repeater(pad_string, Math.ceil(pad_to_go / 2));
             input = half + input + half;
             input = input.substr(0, pad_length);
@@ -336,12 +416,14 @@ String.prototype.pad = function (pad_length, pad_string, pad_type) {
     }
 
     return input;
-}
+};
 
-Object.is = function(x) {
-    return typeof x === 'object' && !(x instanceof Array) && (x instanceof Object);
-}
+Object.is = function (x) {
+    return (
+        typeof x === "object" && !(x instanceof Array) && x instanceof Object
+    );
+};
 
-Object.isPlain = function(x) {
+Object.isPlain = function (x) {
     return Object.is(x) && x.__proto__.__proto__ === null;
-}
+};
